ac80b1e6ff87da872040b34ecac3196c5a14dfa0
Zach <a10135198@gmail.com>
2019-07-18 08:41:38 +0800

    上傳作業

---
+++ Account/Controllers/AccountController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Account.Models;
+using Account.Repository;
+
+
+
+namespace Account.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+       
+        public ActionResult Data()
+        {
+            List<ViewModels> display = new List<ViewModels>();
+            //抓取account方法
+            var repo = new AccountEFRepository();
+            var AccountDB = repo.account();
+
+            //轉換型態
+            for (int i = 0; i < AccountDB.Count; i++)
+            {
+                display.Add(new ViewModels
+                {
+                    Type = (AccountDB[i].Categoryyy == 0) ? "收入" : "支出",
+                    DateTime = AccountDB[i].Dateee,
+                    Money = AccountDB[i].Amounttt,
+                    Number = i+1,
+                });
+            }
+            return View(display);
+        }
+
+       
+      
+
+    }
+}
---
+++ Account/Repository/AccountEFRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Account.Repository
+{
+    public class AccountEFRepository
+    {
+        //撈資料
+        public List<Repository.AccountBook> account()
+        {
+            using (var dbcontext = new SkillTreeHomeworkEntities())
+            {
+                return dbcontext.AccountBook.ToList();
+            }
+        }
+    }
+}
